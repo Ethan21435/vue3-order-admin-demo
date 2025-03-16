0b88aa3d76893a61bc512fe7880834dbda19bde3
Ethan21435 <81737750+Ethan21435@users.noreply.github.com>
2025-03-16 17:52:36 +0800

    Update main.js to import mock service

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
 
+// 导入 Mock 服务
+import './mock'
+
 const app = createApp(App)
 
-app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
